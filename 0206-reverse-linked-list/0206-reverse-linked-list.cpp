fbd24020c01991eeb5a221bde5915d9fb3d4644c
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-08-24 23:53:03 +0530

    Time: 0 ms (100%), Space: 13.3 MB (69.89%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,18 +11,18 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        ListNode* next=NULL;
+        ListNode* prev=NULL;   //here intiallise prev pointer 
+        ListNode* curr=head;    // current pointer
+        ListNode* next=NULL;    //and next pointer
 
-        while(curr!=NULL){
-            next=curr->next;
-            curr->next=prev;
+        while(curr!=NULL){         // loop run until current pointer reaches the null
+            next=curr->next;    //next point initalize current of next
+            curr->next=prev;    //current next reverse the direction so current next intailize with prev(null)
 
-            prev=curr;
-            curr=next;
+            prev=curr;          //after previous goes to current pointer
+            curr=next;          // and current pointer goes to next pointer.
         }
-        return prev;
+        return prev;            //return previous because it points to null pointer 
         
     }
 };
