dd26264145b89090f3bbce69d9955ba94a10cb3e
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-08-26 10:27:55 +0530

    Time: 15 ms (4.06%), Space: 31.4 MB (7.55%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         unordered_map<int,int> count;
-        for(int i:nums)
+        for(int i:nums)   // using map this will count the value of occurence
         count[i]++;
-        for(int i:nums)
+        for(int i:nums)     
         {
-            if(count[i]==1)
-            return i;
+            if(count[i]==1)  //check single occurence number
+            return i;       //return this index;
         }
         return -1;
     }
