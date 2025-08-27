92f8028cad98189c92028eeec995225f74d85c0b
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-08-27 22:36:49 +0530

    Time: 0 ms (100%), Space: 28.3 MB (28.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,34 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cnt=0;
-        int element=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(cnt==0)
-           {
-                cnt=1;
-                element=nums[i];
-           }
-           else if(nums[i]==element)
-           {
-            cnt++;
-           }
-           else
-            cnt--;
-        }
-        int cnt1;
-        for(int i=0;nums.size();i++)
-        {
-            if(nums[i]==element)
-            {
-                cnt1++;
-            }
-            if(cnt1 > (nums.size()/2))
-                return element;
-        }
-        return -1;
-        
-        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n / 2];   // The middle element is always the majority
     }
 };
