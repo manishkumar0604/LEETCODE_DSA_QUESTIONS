b3dce245ba9e28c486ad8a9d4c3090d7b01c4396
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-08-27 22:46:27 +0530

    Update 0169-majority-element.cpp

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());  // here sorting take O(nlogn) timecomplexity
         int n = nums.size();
         return nums[n / 2];   // The middle element is always the majority
     }
