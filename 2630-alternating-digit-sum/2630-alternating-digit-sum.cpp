946e3f060d8e5c72bfa84e7e000a61a92a131c88
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-09-03 02:03:39 +0530

    Time: 0 ms (100%), Space: 8 MB (20.34%) - LeetHub

--- 2630-alternating-digit-sum/2630-alternating-digit-sum.cpp
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int alternateDigitSum(int m) {
-        string s = to_string(m);
-        int sum = 0;
-        for(int i = 0; i<s.size(); i++){
-            if(i%2 == 0) sum = sum + (s[i] - '0');
-            else sum = sum - (s[i] -'0');
+    int alternateDigitSum(int n) {
+        string s=to_string(n);     // here we convert int->string.
+        int sum=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(i%2==0) sum+=(s[i]-'0');   //char->int.
+            else sum-=(s[i]-'0');
         }
         return sum;
+        
     }
 };
