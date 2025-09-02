83cfc608682c92523f1f00eb8fc2b07e14f8534d
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-09-02 08:12:08 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -17,14 +17,14 @@ public:
         // return finalsum;
 
 
-        int maxi=INT_MIN;
+        int maxi=INT_MIN;    //here we create maxi for store maxm sum 
         int n=nums.size();
-        int prefix=0;
+        int prefix=0;        //prefix intialize with 0.
         for(int i=0;i<n;i++)
         {
-            prefix+=nums[i];
+            prefix+=nums[i];    
             maxi=max(maxi,prefix);
-            if(prefix<0)
+            if(prefix<0)           // if less than 0 then prefix intialize with 0 for new subarray because it decrese previous subarr
                 prefix=0;
         }
         return maxi;
