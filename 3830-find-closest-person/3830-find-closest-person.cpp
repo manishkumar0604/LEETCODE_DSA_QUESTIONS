11b2ef3695e17d5a9cbcee0649c0c9d9d441cca1
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-09-05 01:21:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.98%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        // Keep moving until one or both reach z
-        while (true) {
-            if (x == z && y == z) return 0; // both reached
-            if (x == z) return 1;           // x reached first
-            if (y == z) return 2;           // y reached first
-
-            // Move x closer to z
-            if (x < z) x++;
-            else if (x > z) x--;
-
-            // Move y closer to z
-            if (y < z) y++;
-            else if (y > z) y--;
-        }
+        if (abs(x-z)<abs(y-z)) return 1;
+        if (abs(x-z)>abs(y-z)) return 2;
+        else return 0;
+        
     }
 };
