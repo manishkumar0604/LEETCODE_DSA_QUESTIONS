6e3db60015150645e41d635d9fec7b915d420df5
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-09-14 23:56:33 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //remove duplicates element in sorted array to compare next element in array.
     int removeDuplicates(vector<int>& nums) {
         int i=0;
         for(int j=1;j<nums.size();j++)
