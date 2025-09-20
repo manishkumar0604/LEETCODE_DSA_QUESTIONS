d0acf689f1463d8b32dc0954da2d22c7876c17b6
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-09-21 00:00:10 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         for (int i = 0; i < nums.size(); i++) {    //brute force apporach.
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[j] == target - nums[i]) {
-                    return {i, j};
+                    return {i, j}; //return index.
                 }
             }
         }
