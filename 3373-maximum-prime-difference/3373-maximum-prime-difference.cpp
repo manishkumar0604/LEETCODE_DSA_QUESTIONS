b782628b3551fb3311ac7556c9e732ccf723cf77
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-10-01 02:18:25 +0530

    Time: 93 ms (43.99%), Space: 108.6 MB (97.35%) - LeetHub

--- 3373-maximum-prime-difference/3373-maximum-prime-difference.cpp
+++ 3373-maximum-prime-difference/3373-maximum-prime-difference.cpp
@@ -1,29 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-   bool isPrime(int n){
-        if (n <= 1) return false; // numbers <= 1 are not prime
+    bool isPrime(int n){
+        if (n <= 1) return false;
         if (n == 2) return true;
         if (n % 2 == 0) return false;
-        for (int i = 3; i * 1LL * i <= n; i += 2){
-            if (n % i == 0){ // divisible = not prime
-                return false;
-            }
+        for (int i = 3; 1LL * i * i <= n; i += 2){
+            if (n % i == 0) return false;
         }
-        return true; // no divisor = prime
+        return true;
     }
 
     int maximumPrimeDifference(vector<int>& nums) {
-        vector<int> prime;
-        for (int i = 0; i < nums.size(); i++){
+        int prime_size = 0;
+        int min = INT_MAX;
+        int max = INT_MIN;
+
+        for (int i = 0; i < (int)nums.size(); ++i){
             if (isPrime(nums[i])){
-                prime.push_back(i);
+                ++prime_size;
+                if (i < min) min = i;
+                if (i > max) max = i;
             }
         }
 
-        int prime_size = prime.size();
-        if(prime_size == 0) return 0;
-        int min = *min_element(prime.begin(),prime.end());
-        int max = *max_element(prime.begin(),prime.end());
-        return max-min;
+        if (prime_size == 0) return 0;
+        return max - min;
     }
 };
