91d136bbdb43c7c4884a2669568b51cd890ec06c
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-10-04 02:28:32 +0530

    Time: 0 ms (100%), Space: 24.9 MB (85.86%) - LeetHub

--- 3324-split-the-array/3324-split-the-array.cpp
+++ 3324-split-the-array/3324-split-the-array.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
-        int freq[101]={0};
+        vector<int> freq(101,0);
         for (int i=0;i<nums.size();i++) {
             freq[nums[i]]++;
-            if (freq[nums[i]]>2) return false;
+        }
+        for(int i=0;i<freq.size();i++)
+        {
+            if(freq[i]>2) return false;
         }
         return true;
     }
