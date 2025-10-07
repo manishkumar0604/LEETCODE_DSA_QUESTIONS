b30f42da848e0657341347feae15aca458218b9e
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-10-07 16:40:30 +0530

    Time: 0 ms (100%), Space: 66.2 MB (63.42%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
         int ans1=0,ans2=0,n1=nums1.size(),n2=nums2.size();
         vector<int>freq1(101,0),freq2(101,0);
-        for (int i:nums1) {  //count frequency of nums1.
-            freq1[i]++;
+        for (int it:nums1) {  //count frequency of nums1.
+            freq1[it]++;
         }
-        for (int i:nums2) {  //count frequency of nums2.
-            freq2[i]++;
+        for (int it:nums2) {  //count frequency of nums2.
+            freq2[it]++;
         }
-        for (int i:nums2) {  //find if freq1 exist in nums2 if exist the add in ans1.
-            if (freq1[i]>0) ans1++;
+        for (int it:nums2) {  //find if freq1 exist in nums2 if exist the add in ans1.
+            if (freq1[it]>0) ans1++;
         }
-        for (int i:nums1) {   //find if freq2 exist in nums1 if exist the add in ans2.
-            if (freq2[i]>0) ans2++;
+        for (int it:nums1) {   //find if freq2 exist in nums1 if exist the add in ans2.
+            if (freq2[it]>0) ans2++;
         }
         return {ans2,ans1};
     }
