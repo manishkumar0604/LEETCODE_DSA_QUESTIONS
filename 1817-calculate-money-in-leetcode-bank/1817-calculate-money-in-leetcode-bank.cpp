c75be09576bc7c7c5b46d080e5cc76cb8f0af170
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-10-25 16:19:56 +0530

    Time: 0 ms (100%), Space: 8 MB (13.49%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int totalMoney=0;  
+        int weekStart=1;     
+        for(int i=1;i<=n;i++) {
+            totalMoney +=weekStart+(i-1)%7; 
+            if(i%7==0) weekStart++;          
+        }
+        return totalMoney;
+    }
+};
