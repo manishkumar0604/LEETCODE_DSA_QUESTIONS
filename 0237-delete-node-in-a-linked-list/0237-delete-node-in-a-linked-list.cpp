babf3a48397cb4245325cd92af84868e9623f4f7
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-11-01 19:03:48 +0530

    Time: 5 ms (76.88%), Space: 12.3 MB (82.64%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,16 +9,45 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        // Copy value from next node to current node
-        node->val = node->next->val;
+        // Since we don't have access to the head or previous node,
+        // copy the value from the next node into the current one,
+        // then delete the next node.
 
-        // Store next node
-        ListNode* temp = node->next;
-
-        // Skip over the next node
-        node->next = node->next->next;
-
-        // Delete the skipped node
-        delete temp;
+        node->val = node->next->val;          // Step 1: Copy next node's value
+        ListNode* temp = node->next;          // Step 2: Store next node
+        node->next = node->next->next;        // Step 3: Bypass next node
+        delete temp;                          // Step 4: Delete skipped node
     }
 };
+
+
+// class Solution {
+// public:
+//     ListNode* deleteNode(ListNode* head, int node) {
+//         if (head == NULL) return NULL;
+
+//         // If head node is to be deleted
+//         if (head->val == node) {
+//             ListNode* temp = head;
+//             head = head->next;
+//             delete temp;
+//             return head;
+//         }
+
+//         ListNode* temp = head;
+//         ListNode* prev = NULL;
+
+//         // Traverse the list to find the node to delete
+//         while (temp != NULL) {
+//             if (temp->val == node) {
+//                 prev->next = temp->next;
+//                 delete temp;
+//                 break;
+//             }
+//             prev = temp;
+//             temp = temp->next;
+//         }
+
+//         return head;
+//     }
+// };
