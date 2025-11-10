98fd068264dd2ce3beac1c56bb44bc172e363e70
manishkumar0604 <manishkrmfp2018@gmail.com>
2025-11-11 01:36:28 +0530

    Time: 0 ms (100%), Space: 14.9 MB (59.39%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,42 +11,20 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if (head == NULL) return head;
-
-        // Step 1: count total nodes
-        ListNode* temp = head;
-        int count=0;
-        while (temp!=NULL) {
-            count++;
-            temp=temp->next;
+        if(head==NULL) return NULL;
+        ListNode*fast=head;
+        ListNode*slow=head;
+        //fast pointer move n steps intially.
+        for(int i=0;i<n;i++) fast=fast->next;
+        if(fast==NULL) return head->next;  // if n beyond the head size.
+        while(fast->next!=NULL)
+        {
+            fast=fast->next;
+            slow=slow->next;
         }
-
-        // Step 2: find position from start (1-based index)
-        int k = count-n+1;
-
-        // Step 3: if we need to delete the head node
-        if (k==1) {
-           ListNode* temp=head;
-           head=head->next;
-           delete temp;
-           return head;
-        }
-
-        // Step 4: move to the (k-1)th node
-        ListNode* curr=head;
-        int currPos=1;
-        while (currPos < k - 1 && curr != NULL) {
-            curr=curr->next;
-            currPos++;
-        }
-
-        // Step 5: delete the kth node
-        if (curr != NULL && curr->next != NULL) {
-            ListNode* nodeToDelete = curr->next;
-            curr->next = curr->next->next;
-            delete nodeToDelete;
-        }
-
+        ListNode* deleteNode=slow->next;
+        slow->next=slow->next->next;
+        delete deleteNode;
         return head;
     }
 };
