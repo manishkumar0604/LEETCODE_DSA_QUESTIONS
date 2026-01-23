325d09166aabea501bcac6a1a999854c86a3eb8c
manishkumar0604 <manishkrmfp2018@gmail.com>
2026-01-23 20:03:18 +0530

    Time: 2 ms (78.82%), Space: 28.3 MB (79.08%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        vector<int> nge(n, -1);  
-        for(int i=0; i<n; i++)
+        vector<int> nge(n);
+        stack<int> st;
+        for(int i=2*n-1;i>=0;i--)  // we create virtual double the given array
         {
-            for(int j=1; j<n; j++)
-            {  
-                int idx= (i + j) % n;
-                if(nums[idx] > nums[i])
-                {
-                    nge[i]=nums[idx];
-                    break;
-                }
-            }
+            while(!st.empty() && st.top() <= nums[i%n])   
+                st.pop();
+            if(i<n)
+                nge[i]=st.empty() ? -1 : st.top();
+            st.push(nums[i%n]);   // current element become candidate.
         }
         return nge;
     }
